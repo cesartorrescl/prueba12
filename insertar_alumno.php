68a96b4fab93f6468f9b6181486b6b4214fea53f
CesarTorres <cesartorres.cl@gmail.com>
2017-09-13 13:19:17 -0300

    Add files via upload

---
+++ insertar_alumno.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Insertar un nuevo alumno en la base de datos
+ */
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    // Decodificando formato Json
+    $body = json_decode(file_get_contents("php://input"), true);
+
+    // Insertar Alumno
+    
+        
+
+    if ($body['nombre']=="Cesar" && $body['pass']=="clave") {
+        $json_string = json_encode(array("estado" => 1,"mensaje" => "Creacion correcta"));
+		echo $json_string;
+    } else {
+        $json_string = json_encode(array("estado" => 2,"mensaje" => "No se creo el registro"));
+		echo $json_string;
+    }
+}
+
+?>
