0bfd827164ae46d94646f6f922cec51b1aabeb66
CesarTorres <cesartorres.cl@gmail.com>
2017-10-04 03:08:20 -0300

    Add files via upload

---
+++ Alumnos.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * Representa el la estructura de las Alumnoss
+ * almacenadas en la base de datos
+ */
+require 'Database.php';
+
+class Alumnos
+{
+    function __construct()
+    {
+    }
+
+    /**
+     * Retorna en la fila especificada de la tabla 'Alumnos'
+     *
+     * @param $idAlumno Identificador del registro
+     * @return array Datos del registro
+     */
+    public static function getAll()
+    {
+        $consulta = "SELECT * FROM Alumnos";
+        try {
+            // Preparar sentencia
+            $comando = Database::getInstance()->getDb()->prepare($consulta);
+            // Ejecutar sentencia preparada
+            $comando->execute();
+
+            return $comando->fetchAll(PDO::FETCH_ASSOC);
+
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    /**
+     * Obtiene los campos de un Alumno con un identificador
+     * determinado
+     *
+     * @param $idAlumno Identificador del alumno
+     * @return mixed
+     */
+    public static function insert(
+        $nombre,
+        $direccion
+    )
+    {
+        // Sentencia INSERT
+        $comando = "INSERT INTO recoleccion ( " .
+            "patente," .
+            "punto_gps," .
+            "usuario," .
+            "vidrio," .
+            "papel," .
+            "lata," .
+            "carton," .
+            "ruta)" .
+            " VALUES( ?,?,?,?,?,?,?,?)";
+
+        // Preparar la sentencia
+        $sentencia = Database::getInstance()->getDb()->prepare($comando);
+
+        return $sentencia->execute(
+            array(
+                $patente,
+                $punto_gps,
+                $usuario,
+                $vidrio,
+                $papel,
+                $lata,
+                $carton,
+                $ruta
+            )
+        );
+
+    }
+
+
+}
+
+?>
---
+++ insertar_alumno.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Insertar un nuevo alumno en la base de datos
+ */
+
+require 'Alumnos.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    // Decodificando formato Json
+    $body = json_decode(file_get_contents("php://input"), true);
+
+    // Insertar Alumno
+    $retorno = Alumnos::insert(
+        $body['patente'],
+        $body['punto_gps'],
+        $body['usuario'],
+        $body['vidrio'],
+        $body['papel'],
+        $body['lata'],
+        $body['carton'],
+        $body['ruta']);
+
+    if ($retorno) {
+        $json_string = json_encode(array("estado" => 1,"mensaje" => "Creacion correcta"));
+		echo $json_string;
+    } else {
+        $json_string = json_encode(array("estado" => 2,"mensaje" => "No se creo el registro"));
+		echo $json_string;
+    }
+}
+
+?>
