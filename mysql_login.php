599cab7bfebb8b3c0a7375d8f23270a4b6698bb9
CesarTorres <cesartorres.cl@gmail.com>
2017-10-04 02:55:03 -0300

    Add files via upload

---
+++ mysql_login.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Provee las constantes para conectarse a la base de datos
+ * Mysql.
+ */
+define("HOSTNAME", "ec2-184-73-189-190.compute-1.amazonaws.com");// Nombre del host
+define("DATABASE", "d3lk9mi0tke8o6"); // Nombre de la base de datos
+define("USERNAME", "wxxpmfagcsipwr"); // Nombre del usuario
+define("PASSWORD", "65906187efc11420df849f00d1e663132bc55e214cf698cb6bc497c726be5536"); // Nombre de la constraseña
+?>
